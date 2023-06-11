49f2cec2d83d5afea7920736819d653ec580356a
lancej1022 <lancej1022@gmail.com>
2023-06-11 12:00:57 -0700

    Fix socials and remove dead code

--- src/utils/readingTime.ts
+++ src/utils/readingTime.ts
@@ -4,10 +4,10 @@ const AVERAGE_WORDS_READ_PER_MINUTE = 200;
 
 export function getReadingTime(post: CollectionEntry<"blog">) {
   if (!post.body) return;
-  console.log({ post });
+
   // TODO: improve regex
   const clean = post.body.replace(/<\/?[^>]+(>|$)/g, "");
-  console.log({ clean });
+  //  TODO: improve calculation -- kinda inaccurate because the parsed string has junk in it
   const numberOfWords = clean.split(/\s/g).length;
 
   return Math.ceil(numberOfWords / AVERAGE_WORDS_READ_PER_MINUTE);
