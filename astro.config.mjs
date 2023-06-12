7dfe9452e278074e6ee0848ff52418ffb5164c2b
lancej1022 <lancej1022@gmail.com>
2023-06-12 10:52:29 -0700

    Comment out `satori` to avoid bug during build step.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,42 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+
 import { SITE } from "./src/config";
 
-import solidJs from "@astrojs/solid-js";
-
-// https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), sitemap(), solidJs()],
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    sitemap(),
+    solidJs(),
+  ],
   markdown: {
-    remarkPlugins: [remarkToc, [remarkCollapse, {
-      test: "Table of contents"
-    }]],
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: "Table of contents",
+        },
+      ],
+    ],
     shikiConfig: {
       theme: "one-dark-pro",
-      wrap: true
+      wrap: true,
     },
-    extendDefaultPlugins: true
+    extendDefaultPlugins: true,
   },
   vite: {
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"]
-    }
-  }
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
 });
