feffc7363a40b5490d35ce58742b56fa43165920
Lance Jeffers <lancejeffers@Lances-MacBook-Air.local>
2023-06-14 00:42:44 -0700

    Implement holy grail styles on PostDetails page

--- src/utils/readingTime.ts
+++ src/utils/readingTime.ts
@@ -1,12 +1,10 @@
-import type { CollectionEntry } from "astro:content";
-
 const AVERAGE_WORDS_READ_PER_MINUTE = 200;
 
-export function getReadingTime(post: CollectionEntry<"blog">) {
-  if (!post.body) return;
+export function getReadingTime(post: string) {
+  if (!post) return;
 
   // TODO: improve regex
-  const clean = post.body.replace(/<\/?[^>]+(>|$)/g, "");
+  const clean = post.replace(/<\/?[^>]+(>|$)/g, "");
   //  TODO: improve calculation -- kinda inaccurate because the parsed string has junk in it
   const numberOfWords = clean.split(/\s/g).length;
 
